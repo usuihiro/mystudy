c504a295ad4d96f6069f449d84a92c5fc934a959
Hirofumi Usui <usui.hirofumi@dena.jp>
2013-12-25 19:11:20 +0900

    Initial Commit

---
+++ ios_app/StoryBoardStudy/StoryBoardStudy/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoryBoardStudy
+//
+//  Created by Hirofumi Usui on 2013/12/25.
+//  Copyright (c) 2013年 EVERYSTAR Co,.LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
